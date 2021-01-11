ae07857c5be2e2b02d41fee150483f4a701d25a8
rlespinasse <romain.lespinasse@gmail.com>
2021-01-11 22:23:54 +0100

    fix: support arguments with space

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ export DISPLAY=:9.0
 # Start runner and enable signals to it
 trap 'kill -TERM $PID' TERM INT
 # shellcheck disable=SC2068
-/drawio/runner.sh $@ &
+/drawio/runner.sh "$@" &
 PID=$!
 wait $PID
 trap - TERM INT
